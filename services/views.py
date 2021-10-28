460252b21be7fac426ce56caa5d2050a19c2b910
Ulugbek <ulugbekmuslitdinov@gmail.com>
2021-10-28 14:14:30 +0500

    services model added

--- services/views.py
+++ services/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
+from .models import Service
 
 
 def index(request):
-    return render(request, 'index.html')
+    services = Service.objects.all()
+    context = {'services': services}
+    return render(request, 'index.html', context)
